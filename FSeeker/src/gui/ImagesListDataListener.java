83ed47ec89c9c385f71469458fce76027fb43fd5
brahima <brahima>
2004-10-17 18:01:38 +0000

    classe séparée

--- FSeeker/src/gui/ImagesListDataListener.java
+++ FSeeker/src/gui/ImagesListDataListener.java
@@ -6,55 +6,56 @@ import javax.swing.event.ListDataListener;
 
 import model.ListImagesModel;
 
+
+/**
+ * Created on 16 oct. 2004 
+ * @author brahim
+ *
+ */
 /**
  * @author brahim
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 class ImagesListDataListener implements ListDataListener {
 
-	private ListImagesModel m;
-	private JList list;
-	public ImagesListDataListener(ListImagesModel m, JList list){
-		this.m = m;
-		this.list = list;
-		m.addListDataListener(this);
-	}
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.swing.event.ListDataListener#contentsChanged(javax.swing.event.ListDataEvent)
-	 */
-	public void contentsChanged(ListDataEvent e) {
-		// TODO Auto-generated method stub
-		System.out.println("Changement");		
-		list.clearSelection();
-		list.ensureIndexIsVisible(0);//utile ?
-		list.setModel(m);
-		list.revalidate();
-		list.repaint();
+    private ListImagesModel m;
 
-	}
+    private JList list;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.swing.event.ListDataListener#intervalAdded(javax.swing.event.ListDataEvent)
-	 */
-	public void intervalAdded(ListDataEvent e) {
-		// TODO Auto-generated method stub
+    public ImagesListDataListener(ListImagesModel m, JList list) {
+        this.m = m;
+        this.list = list;
+        m.addListDataListener(this);
+    }
 
-	}
+   /**
+    * @param 
+    * 
+    * Ce type d'évenement est appelé lorsque le modéle de la Jlist associé change
+    * dans notre cas on change de répertoire ou autre, on met donc à jour le tout
+    */
+    public void contentsChanged(ListDataEvent e) {
+        System.out.println("Changement");
+        list.clearSelection();
+        list.ensureIndexIsVisible(0);//utile ?
+        list.setModel(m);
+        list.revalidate();
+        list.repaint();
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see javax.swing.event.ListDataListener#intervalRemoved(javax.swing.event.ListDataEvent)
-	 */
-	public void intervalRemoved(ListDataEvent e) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    /**
+     *  Cette méthode n'est pas implémentée, elle ne fait rien de particulier
+     */
+    public void intervalAdded(ListDataEvent e) {
+
+    }
+
+    /**
+     *  Cette méthode n'est pas implémenté, elle ne fait rien de particulier
+     */
+    public void intervalRemoved(ListDataEvent e) {
+
+    }
 
 }
