5655da56739233319fae98afe59bf5bc74cf9c15
sted <sted>
2004-10-23 17:51:26 +0000

    modif pour la gestion des extensions

--- FSeeker/src/misc/ImagesMap.java
+++ FSeeker/src/misc/ImagesMap.java
@@ -5,6 +5,7 @@ package misc;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.StringTokenizer;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -16,78 +17,74 @@ import javax.swing.ImageIcon;
  * @author aitelhab
  */
 public class ImagesMap {
-    /** L'image par défaut */
-    private static final String DEFAULT_IMAGE = "default.png";
+	/** L'image par défaut */
+	private static final String DEFAULT_IMAGE = "default.png";
 
-    /**
-     * Le tableau associatif contenant les images. <br>
-     * [String clef] => [Icon ressource]
-     */
-    protected static final HashMap images = new HashMap();
+	/**
+	 * Le tableau associatif contenant les images. <br>
+	 * [String clef] => [Icon ressource]
+	 */
+	protected static final HashMap images = new HashMap();
 
-    /** Le séparateur de fichier */
-    private static final char SEP = File.separatorChar;
+	/** Le séparateur de fichier */
+	private static final char SEP = File.separatorChar;
 
-    /** Là où sont stockées toutes les images */
-    private static final String IMAGES = "images"+SEP+"extensions";
+	/** Là où sont stockées toutes les images */
+	private static final String IMAGES = "images", EXTENSIONS = "extensions";
 
-    /**
-     * Renvoie un objet Icon à partir du chemin de image. Si le chemin n'est pas
-     * valide ou que l'image n'existe pas, renvoie null.
-     * 
-     * @param image
-     *            le chemin de l'image (ie: "dot.gif");
-     * @return la ressource Icon
-     */
-    public static Icon get(String image) {
-        // Si on l'a déjà chargée, on renvoie celle de la hashmap
-        if (images.containsKey(image))
-            return (Icon) images.get(image);
+	/**
+	 * Renvoie un objet Icon à partir du chemin de image. Si le chemin n'est pas
+	 * valide ou que l'image n'existe pas, renvoie null.
+	 * 
+	 * @param image
+	 *            le chemin de l'image (ie: "dot.gif");
+	 * @return la ressource Icon
+	 */
+	public static Icon get(String image) {
+		// Si on l'a déjà chargée, on renvoie celle de la hashmap
+		if (images.containsKey(image))
+			return (Icon) images.get(image);
 
-        // Sinon on la charge dans la hasp et on renvoie
-        String chemin = IMAGES + SEP + image;
-        if (new File(chemin).exists()) {
-            Icon pic = new ImageIcon(chemin);
-            images.put(image, pic);
-            return pic;
-        }
+		// Sinon on la charge dans la hasp et on renvoie
+		String chemin = IMAGES + SEP + image;
+		if (new File(chemin).exists()) {
+			Icon pic = new ImageIcon(chemin);
+			images.put(image, pic);
+			return pic;
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    public static Icon getIcon(File file) {
-        String s = file.getName();
-        Icon fileIcon = null;
-        String fileStr = file.toString();
+	/**
+	 * Renvoie un objet Icon à partir d'un fichier. L'Icon renvoyé dépendra de
+	 * l'extension de ce fichier. Si l'extension n'a pas d'image associée,
+	 * l'image par défaut est renvoyée.
+	 * 
+	 * @param file
+	 *            le fichier
+	 * @return la ressource Icon
+	 */
+	public static Icon getImage(File file) {
+		// Image spéciale pour tous les répertoires
+		if (file.isDirectory())
+			return get("directory.png");
 
-        if (file.isDirectory()) {
-            fileIcon = get("directory.png");
-        } else {
+		// On a un fichier, on regarde son extension		
+		String s = file.getName();
+		int rindex = s.lastIndexOf('.');		
 
-            int rindex = fileStr.lastIndexOf('.');
+		// Y'a pas de '.' ou bien on a un fichier genre : "truc."
+		if (rindex == -1 || rindex + 1 >= s.length())
+			return getDefault();
+		
+		String ext = s.substring(rindex + 1);
+		return get(EXTENSIONS + SEP + ext + ".png");
+	}
 
-            String ext = null;
-            if (rindex >= 0)
-                ext = fileStr.substring(rindex + 1);
-
-            if (ext != null)
-                fileIcon = get(ext + ".png");
-            if (fileIcon == null)
-                fileIcon = getDefault();
-            
-        }
-        return fileIcon;
-    }
-
-    private  Icon getImage(String ext) {
-        if ("".equals(ext))
-            return getDefault();
-        return get(ext + ".png");
-    }
-
-    public static Icon getDefault() {
-        return get(DEFAULT_IMAGE);
-    }
+	public static Icon getDefault() {
+		return get(DEFAULT_IMAGE);
+	}
 }
 
 ////////
