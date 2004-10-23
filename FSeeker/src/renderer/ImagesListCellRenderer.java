8c4760f949f87b28ca78aa3cf789047eec958a1c
brahima <brahima>
2004-10-23 12:01:15 +0000

    Voir le TODO à l'intérieur

--- FSeeker/src/renderer/ImagesListCellRenderer.java
+++ FSeeker/src/renderer/ImagesListCellRenderer.java
@@ -14,32 +14,18 @@ public class ImagesListCellRenderer extends DefaultListCellRenderer {
     
     public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
 		super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
-	    // TODO Refaire cette méthode au propre
+	    
 		File file = (File) value;
 		String s = file.getName();
-		
-		Icon fileIcon = null;
-		String fileStr = file.toString();
-		
-		if (file.isDirectory()) {				
-			fileIcon = ImagesMap.get("directory.png");
-			setText(s.toUpperCase());
-		} else {
-		    setText(s.toLowerCase());
-		    
-		    int rindex = fileStr.lastIndexOf('.');
-		    
-		    String ext = null;
-		    if (rindex >= 0)
-		        ext = fileStr.substring(rindex);
-		    
-			if (ext != null) {
-				fileIcon = ImagesMap.get(ext + ".png");
-			}
-			
-		    if (fileIcon == null)
-		        fileIcon = ImagesMap.get("dot.gif");
-		}	
+        Icon fileIcon = ImagesMap.getIcon(file);
+        
+        //Ca fait moche ca et puis si le systéme de fichier respecte la casse ca le fait plus
+        //TODO A retirer plus tard
+        if (file.isDirectory()) {            
+            setText(s.toUpperCase());
+        } else {
+            setText(s.toLowerCase());            
+        }	
 		
 		setIcon(fileIcon);
 
