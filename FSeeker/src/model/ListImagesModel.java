22857cc6130a9d9c5206a8487afa938ccc1937bd
sted <sted>
2004-10-23 19:48:33 +0000

    rajout de +1 -1 et autre pour afficher le rép parent

--- FSeeker/src/model/ListImagesModel.java
+++ FSeeker/src/model/ListImagesModel.java
@@ -17,50 +17,54 @@ import javax.swing.event.ListDataListener;
  */
 public class ListImagesModel implements ListModel {
 
-    private File dir;
+	private File dir = null;
 
-    private ArrayList listeners;
+	private ArrayList listeners = new ArrayList();
 
-    public ListImagesModel(File dir) {
-        this.dir = dir;
-        listeners = new ArrayList(0);
-    }
+	public ListImagesModel(File dir) {
+		this.dir = dir;
+	}
 
-    /**
-     * Renvoie le nombre de fichiers dans le dossier
-     */
-    public int getSize() {
-        return dir.list().length;
-    }
+	/**
+	 * Renvoie le nombre de fichiers dans le dossier
+	 */
+	public int getSize() {
+		return dir.list().length + 1;
+	}
 
-    /**
-     * the file at the specified index in the directory
-     */
-    public Object getElementAt(int index) {
-        File[] files = dir.listFiles();
-        return files[index];
-    }
+	/**
+	 * the file at the specified index in the directory
+	 */
+	public Object getElementAt(int index) {
+		if (index == 0) {
+			if (dir.getParentFile() == null)
+				return dir;
+			return dir.getParentFile();
+		}
+		File[] files = dir.listFiles();
+		return files[index-1];
+	}
 
-    public void addListDataListener(ListDataListener l) {
-        if (!listeners.contains(l) && l != null)
-            listeners.add(l);
-    }
+	public void addListDataListener(ListDataListener l) {
+		if (!listeners.contains(l) && l != null)
+			listeners.add(l);
+	}
 
-    public void removeListDataListener(ListDataListener l) {
-        if (l != null)
-            listeners.remove(l);
-    }
+	public void removeListDataListener(ListDataListener l) {
+		if (l != null)
+			listeners.remove(l);
+	}
 
-    public void setDirectory(File dir) {
-        this.dir = dir;
-        fire(new ListDataEvent(this, ListDataEvent.CONTENTS_CHANGED, 0,
-                getSize() - 1));
-    }
+	public void setDirectory(File dir) {
+		this.dir = dir;
+		fire(new ListDataEvent(this, ListDataEvent.CONTENTS_CHANGED, 0,
+				getSize() - 1));
+	}
 
-    //	Indique à tous les écouteurs, qu'il y a eut un changement
-    private void fire(ListDataEvent l) {
-        Iterator it = listeners.iterator();
-        while (it.hasNext())
-            ((ListDataListener) it.next()).contentsChanged(l);
-    }
+	//	Indique à tous les écouteurs, qu'il y a eut un changement
+	private void fire(ListDataEvent l) {
+		Iterator it = listeners.iterator();
+		while (it.hasNext())
+			((ListDataListener) it.next()).contentsChanged(l);
+	}
 }
