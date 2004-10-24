c5b2bdf97ee756b8a39a52350ecd1e9c8373c79c
sted <sted>
2004-10-24 13:38:11 +0000

    gère la vue liste simple/icone

--- FSeeker/src/gui/ListImagesGUI.java
+++ FSeeker/src/gui/ListImagesGUI.java
@@ -11,18 +11,23 @@ import controler.ListImagesMouseListener;
 
 /**
  * @author brahim
+ * @author Sted
  */
 public class ListImagesGUI extends JList {
 
     private ListImagesModel m = null;
 
     public ListImagesGUI(ListImagesModel m) {
-        this.m = m;
+        this(m, false);
+    }
+    
+    public ListImagesGUI(ListImagesModel m, boolean simple) {
+    	this.m = m;
         setModel(m);
         setDragEnabled(true);
         setLayoutOrientation(JList.HORIZONTAL_WRAP);
         setLayout(new FlowLayout());
-        setCellRenderer(new ListImagesCellRenderer());
+        setCellRenderer(new ListImagesCellRenderer(simple));
 
         new ListImagesDataControler(m, this);
         addMouseListener(new ListImagesMouseListener());
