651be2d67129cb788015d90866eec8ee4ae582cc
sted <sted>
2004-10-24 13:37:34 +0000

    pour la vue en arbre

---
+++ FSeeker/src/misc/FileSystemTree.java
@@ -0,0 +1,32 @@
+/*7
+ * Created on 24 oct. 2004
+ */
+package misc;
+
+import java.io.File;
+import java.util.LinkedList;
+
+import javax.swing.tree.TreePath;
+
+/**
+ * @author sted
+ */
+public class FileSystemTree {
+	public static TreePath getTreePath(File f) {
+
+		LinkedList filesList = new LinkedList();
+		File g = new File(f.toString());
+
+		while (g != null) {
+			filesList.addLast(g);
+			g = g.getParentFile();
+		}
+
+		TreePath treepath = new TreePath(filesList.removeLast());
+
+		while (!filesList.isEmpty())
+			treepath = treepath.pathByAddingChild(filesList.removeLast());
+
+		return treepath;
+	}
+}
