0fb3217928f4ea55794f54c90e22afc26f1deba6
brahima <brahima>
2004-10-28 15:14:24 +0000

    Quelque modifs

--- FSeeker/src/renderer/FileTableCellRenderer.java
+++ FSeeker/src/renderer/FileTableCellRenderer.java
@@ -13,59 +13,79 @@ import javax.swing.Icon;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
+//import misc.FileUtilities;
+
 import misc.ImagesMap;
-import misc.file.FileUtilities;
+//import java.util.*;
+
+//import misc.file.FileUtilities;
+
 
 /**
  * 
  * @author aitelhab
- * @author Sted  
+ * @author Sted
  */
 public class FileTableCellRenderer extends DefaultTableCellRenderer {
 
-	public FileTableCellRenderer() {
-		//super();
-	}
+    public FileTableCellRenderer() {
+        super();
+    }
 
-	public Component getTableCellRendererComponent(JTable table, Object value,
-			boolean isSelected, boolean hasFocus, int row, int column) {
+    public Component getTableCellRendererComponent(JTable table, Object value,
+            boolean isSelected, boolean hasFocus, int row, int column) {
 
-		DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) table
-				.getCellRenderer(row, column);
+        DefaultTableCellRenderer renderer = (DefaultTableCellRenderer) table
+                .getCellRenderer(row, column);
 
-		Color c = table.getBackground();
-		//Couleur à personaliser ,on verra ca plus tard
-		if ((row % 2) == 0 && c.getRed() > 40 && c.getGreen() > 30
-				&& c.getBlue() > 10)
-			setBackground(new Color(c.getRed() - 40, c.getGreen() - 30, c
-					.getBlue() - 10));
-		else
-			setBackground(c);
+        File file = null;
+        if (value instanceof File && value != null)
+            file = (File) value;
+        if (file != null) {
+            Color c = table.getBackground();
+            //Couleur à personaliser ,on verra ca plus tard
+            if ( (column == 0) && (row % 2) == 0 && c.getRed() > 30 && c.getGreen() > 20
+                    && c.getBlue() > 20 )
+                setBackground(new Color(c.getRed() - 30 , c.getGreen() - 20, c
+                        .getBlue() - 20));
+            else
+                setBackground(c);
+        }
+        
+        if(isSelected){
+            setBackground(new Color(115,118, 113));
+           
+            //setForeground(Color.white);
+        }
 
-		setValue(value);
+        if (value != null)
+            setValue(value);
 
-		return renderer;
-	}
+        return this;
+    }
 
-	/**
-	 * Surcharge de la méthode setValue avec traitement différent en fonction de
-	 * la classe de l'objet value
-	 * 
-	 * @param value
-	 *            l'objet dont on veut personaliser l'affichage
-	 */
-	public void setValue(Object value) {
-		Class cl = value.getClass();
-		if (cl == File.class) {
-			File file = (File) value;
-			//JLabel lab = new JLabel();
-			Icon fileIcon = ImagesMap.get16x16(file);
-			//lab.setIcon(fileIcon);
-			setIcon(fileIcon);
-			super.setValue(file.getName() + (file.isDirectory() ? "/" : ""));
-			setToolTipText(FileUtilities.getDetails(file));
-			//super.setValue( file.getName() ) ;
-		}
-	}
+    /**
+     * Surcharge de la méthode setValue avec traitement différent en fonction de
+     * la classe de l'objet value
+     * 
+     * @param value
+     *            l'objet dont on veut personaliser l'affichage
+     */
+    public void setValue(Object value) {
+
+        Class cl = value.getClass();
+        if (cl == File.class) {
+            File file = (File) value;
+            
+            
+                Icon fileIcon = ImagesMap.get16x16(file);
+                setIcon(fileIcon); 
+                          
+            setValue(file.getName() /*+ (file.isDirectory() ? "/" : "")*/);
+            setToolTipText(null);//FileUtilities.getDetails(file));            
+        } else
+            super.setValue(value);
+
+    }
 
 }
