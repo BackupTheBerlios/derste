fdb1ba51af4242e97317b138743cdacb04ad96cc
sted <sted>
2004-10-28 13:09:42 +0000

    nettoyage + javadoc + optim

--- FSeeker/src/model/ListImagesModel.java
+++ FSeeker/src/model/ListImagesModel.java
@@ -4,97 +4,109 @@
 package model;
 
 import java.io.File;
-import java.util.Comparator;
 import java.util.Observable;
 import java.util.Observer;
-import java.util.Set;
 import java.util.TreeSet;
 
 import javax.swing.ListModel;
 import javax.swing.event.ListDataListener;
 
-import misc.file.CompareByType;
-
 /**
+ * Représente le modèle d'une liste de fichiers.
+ * 
  * @author Sted
  * @author brahim
  */
 public class ListImagesModel extends Observable implements ListModel, Observer {
 
-    /** Le supra-modèle */
-    protected FSeekerModel fsm = null;
+	/** Le supra-modèle */
+	protected FSeekerModel fsm = null;
 
-    /** Le comparateur utilisé */
-    private Comparator currentComparator = CompareByType.get();
+	/**
+	 * L'ensemble trié contenant les fichiers du répertoire courant
+	 */
+	protected TreeSet filesList = null;
 
-    /** L'ensemble trié contenant les fichiers du répertoire courant */
-    protected Set liste = new TreeSet(currentComparator);
+	/**
+	 * Construit un modèle de liste de fichiers.
+	 * 
+	 * @param fsm
+	 *            le supra-modèle
+	 */
+	public ListImagesModel(FSeekerModel fsm) {
+		this.fsm = fsm;
+		fsm.addObserver(this);
 
-    /**
-     * Construit un modèle de liste de fichiers.
-     * 
-     * @param fsm
-     *            le supra-modèle
-     */
-    public ListImagesModel(FSeekerModel fsm) {
-        this.fsm = fsm;
-        fsm.addObserver(this);
-        refreshListFiles();
-    }
+		updateFilesList();
+	}
 
-    /**
-     * Retourne le supra-modèle.
-     * 
-     * @return supra-modèle
-     */
-    public FSeekerModel getModel() {
-        return fsm;
-    }
+	/**
+	 * Retourne le supra-modèle.
+	 * 
+	 * @return supra-modèle
+	 */
+	public FSeekerModel getModel() {
+		return fsm;
+	}
 
-    public void update(Observable o, Object caller) {
-        // Le modèle peut s'auto changer, il est à la fois modèle et contrôleur
-        // !
-        refreshListFiles();
-        setChanged();
-        notifyObservers(caller);
-    }
+	public void update(Observable o, Object caller) {
+		// Le modèle peut s'auto changer, il est à la fois modèle et contrôleur
 
-    /**
-     * Met à jour l'attribut <code>liste</code> (contenant les noms de
-     * fichiers du répertoire courant).
-     */
-    protected void refreshListFiles() {
-        liste.clear();
-        File[] files = fsm.getURI().listFiles();
-        if (files != null)
-            for (int i = 0; i < files.length; i++)
-            	if (!files[i].isHidden() || getModel().showHidden())
-            		liste.add(files[i]);
-    }
+		// Si c'est le comparator qui a changé
+		if (fsm.isChanged(FSeekerModel.COMPARATOR)) {
+			TreeSet newList = new TreeSet(fsm.getComparator());
+			newList.addAll(filesList);
+			filesList.clear();
+			filesList = newList;
+		} else { // Si c'est l'uri, le showHidden ou autre..
+			updateFilesList();
+		}
 
-    /**
-     * Renvoie le nombre de fichiers dans le dossier courant.
-     * 
-     * @return nombre de fichiers dans le dossier courant
-     */
-    public int getSize() {
-        return liste.size();
-    }
+		setChanged();
+		notifyObservers(caller);
+	}
 
-    /**
-     * Renvoie l'élément à l'index <code>index</code> dans la liste.
-     * 
-     * @param index l'index de l'objet désiré
-     * @return l'objet à l'index désiré
-     */
-    public Object getElementAt(int index) {
-        return liste.toArray()[index];
-    }
+	/**
+	 * Met à jour l'attribut <code>filesList</code> (contenant les noms de
+	 * fichiers du répertoire courant).
+	 */
+	protected void updateFilesList() {
+		if (filesList == null)
+			filesList = new TreeSet(fsm.getComparator());
+		else
+			filesList.clear();
 
-    public void addListDataListener(ListDataListener l) {
-    }
+		File[] files = fsm.getURI().listFiles();
+		if (files != null)
+			for (int i = 0; i < files.length; i++)
+				if (!files[i].isHidden() || getModel().showHidden())
+					filesList.add(files[i]);
+	}
 
-    public void removeListDataListener(ListDataListener l) {
-    }
+	/**
+	 * Renvoie le nombre de fichiers dans le dossier courant.
+	 * 
+	 * @return nombre de fichiers dans le dossier courant
+	 */
+	public int getSize() {
+		return filesList.size();
+	}
+
+	/**
+	 * Renvoie l'élément à l'index <code>index</code> dans la filesList.
+	 * 
+	 * @param index
+	 *            l'index de l'objet désiré
+	 * @return l'objet à l'index désiré
+	 */
+	public Object getElementAt(int index) {
+		return filesList.toArray()[index];
+	}
+
+	public void addListDataListener(ListDataListener l) {
+	}
+
+	public void removeListDataListener(ListDataListener l) {
+	}
 
 }
