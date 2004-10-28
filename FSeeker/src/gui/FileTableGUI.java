b376791e8af253c421b7d465a4900b9bc8b5f4b1
brahima <brahima>
2004-10-28 16:05:44 +0000

    4 colonnes pour le model ( fichier, taille ..)

--- FSeeker/src/gui/FileTableGUI.java
+++ FSeeker/src/gui/FileTableGUI.java
@@ -6,6 +6,9 @@ package gui;
 
 import javax.swing.*;
 import javax.swing.table.*;
+
+import controler.*;
+
 import java.awt.*;
 import java.util.*;
 import java.io.*;
@@ -17,26 +20,38 @@ import renderer.*;
  */
 public class FileTableGUI extends JTable implements Observer{
 	
-	private DefaultTableModel m;
+	private TableModel m;
 	
 	public FileTableGUI(FileTableModel m){
-		this.m = m.getModel();
+		this.m = m.getTableModel();
+		m.addObserver(this);
 		this.setDefaultRenderer(Object.class, new FileTableCellRenderer());
 		setShowGrid(false);
-		
+		setAutoResizeMode(JTable.AUTO_RESIZE_NEXT_COLUMN);	
+		//setRowHeight(25); 
+		//setRowMargin(5); 
+		setDragEnabled(true);		
+		//setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		//setCellSelectionEnabled(true); 
+        setLayout(new FlowLayout());
+		addMouseListener(new TableModelControler(m));
 		setModel(this.m);			
 	}
 	
 	public void update(Observable o, Object arg){
+	    System.out.println("update() de la JTable appelé");
 		//Mettre à jour la vue
+	    revalidate();
+        repaint();
 	}
 	
-	public static void main(String [] args){		
-		FileTableModel model = new FileTableModel(new File("/home/brahim/"), "Colonne Fichier");
+	public static void main(String [] args){
+	    FSeekerModel fsm = new FSeekerModel(new File("/home/brahim/"));
+	    FileTableModel model = new FileTableModel(fsm);
 		FileTableGUI vue = new FileTableGUI(model);
 		JFrame frame = new JFrame("Test vue détail");
 		
-		JScrollPane jsp = new JScrollPane(vue);
+		JScrollPane jsp = new JScrollPane(vue);	
 		frame.getContentPane().add(jsp);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
