f20f61dfcaac1e8ad73c6cf30223b92c757f93bc
sted <sted>
2004-10-29 15:08:23 +0000

    gestion des tooltips

--- FSeeker/src/gui/FileSystemTreeGUI.java
+++ FSeeker/src/gui/FileSystemTreeGUI.java
@@ -3,13 +3,17 @@
  */
 package gui;
 
+import java.awt.Point;
+import java.awt.event.MouseEvent;
 import java.io.File;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JTree;
+import javax.swing.ToolTipManager;
 
 import misc.FileSystemTree;
+import misc.file.FileUtilities;
 import model.FileSystemTreeModel;
 import renderer.FileSystemTreeCellRenderer;
 import controler.FileSystemTreeControler;
@@ -28,7 +32,13 @@ public class FileSystemTreeGUI extends JTree implements Observer {
         setModel(m);
         setEditable(true);
         setCellRenderer(new FileSystemTreeCellRenderer());
-        addTreeSelectionListener(new FileSystemTreeControler(m));
+        
+        // Par défaut, le JTree ne s'enregistre pas
+        ToolTipManager.sharedInstance().registerComponent(this);
+
+        FileSystemTreeControler fstc = new FileSystemTreeControler(m);
+        addTreeSelectionListener(fstc);
+        addKeyListener(fstc);
     }
 
     public void update(Observable o, Object caller) {
@@ -38,6 +48,20 @@ public class FileSystemTreeGUI extends JTree implements Observer {
 
     public void setDirectory(File dir) {
         setSelectionPath(FileSystemTree.getTreePath(dir));
+    }
+
+    /**
+     * Affiche le tooltip évalué dynamiquement. Appelée automatiquement par
+     * Swing.
+     */
+    public String getToolTipText(MouseEvent event) {
+        Point clic = event.getPoint();
+        if (getRowForLocation(clic.x, clic.y) != -1) {
+            File f = (File) getPathForLocation(clic.x, clic.y)
+                    .getLastPathComponent();
+            return FileUtilities.getDetails(f);
+        }
+        return null;
     }
 
 }
