c2bdf76195d99671d14c15a59175c6f30494a98f
sted <sted>
2004-10-31 17:34:53 +0000

    improvements

--- FSeeker/src/model/FileTableModel.java
+++ FSeeker/src/model/FileTableModel.java
@@ -11,16 +11,11 @@ import java.util.Observable;
 import java.util.Observer;
 import java.util.Vector;
 
-import java.util.*;
-import gui.FileTableGUI;
 import javax.swing.JTable;
-import javax.swing.event.*;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
+import misc.file.FileUtilities.FileDetails;
 
 /**
  * Classe qui permet la création d'un modèle de système de fichier pour la
@@ -31,234 +26,229 @@ import javax.swing.table.TableModel;
  */
 public class FileTableModel extends DefaultTableModel implements Observer {
 
-    public static int SIMPLE_MODE = 0;
+	public static int SIMPLE_MODE = 0;
 
-    public static int SPECIAL_MODE = 1;
+	public static int SPECIAL_MODE = 1;
 
-    private int MODE = -1;
+	private int MODE = -1;
 
-    /* La derniére colonne selectionnée */
-    private int lastSelectedColumn = -1;
+	/* La derniére colonne selectionnée */
+	private int lastSelectedColumn = -1;
 
-    protected Vector colNames = new Vector(4);
+	protected Vector colNames = new Vector(4);
 
-    {
-        colNames.addElement("Nom du fichier");
-        colNames.addElement("Taille");
-        colNames.addElement("Type");
-        colNames.addElement("Date de Modification");
-    }
+	{
+		colNames.addElement("Nom du fichier");
+		colNames.addElement("Taille");
+		colNames.addElement("Type");
+		colNames.addElement("Date de Modification");
+	}
 
-    protected FSeekerModel fsm = null;
+	protected FSeekerModel fsm = null;
 
-    /**
-     * Contructeur d'un model
-     * 
-     * @param fsm
-     *            Le supra-modèle associé
-     * @param mode
-     *            Le mode choix entre FileTableModel.SPECIAL_MODE et
-     *            FileTableModel.SIMPLE_MODE
-     */
-    public FileTableModel(FSeekerModel fsm, int mode) {
-        this.fsm = fsm;
-        fsm.addObserver(this);
-        this.MODE = mode;
+	/**
+	 * Contructeur d'un model
+	 * 
+	 * @param fsm
+	 *            Le supra-modèle associé
+	 * @param mode
+	 *            Le mode choix entre FileTableModel.SPECIAL_MODE et
+	 *            FileTableModel.SIMPLE_MODE
+	 */
+	public FileTableModel(FSeekerModel fsm, int mode) {
+		this.fsm = fsm;
+		fsm.addObserver(this);
+		this.MODE = mode;
 
-        if (MODE == SPECIAL_MODE)
-            setDataForSpecialView();
-        else {
-            setColumnIdentifiers(colNames);
-            setDataForSimpleView();
-        }
+		if (MODE == SPECIAL_MODE)
+			setDataForSpecialView();
+		else {
+			setColumnIdentifiers(colNames);
+			setDataForSimpleView();
+		}
 
-    }
+	}
 
-    public FSeekerModel getModel() {
-        return fsm;
-    }
+	public FSeekerModel getModel() {
+		return fsm;
+	}
 
-    public void update(Observable o, Object arg) {
-        System.out.println("MODEL PRINCIPAL A CHANGE");
+	public void update(Observable o, Object arg) {
+		//Mise à jour des données
+		if (MODE == SPECIAL_MODE)
+			setDataForSpecialView();
+		else
+			setDataForSimpleView();
+		//fireTableStructureChanged();
+		//fireTableDataChanged();
 
-        //Mise à jour des données
-        if (MODE == SPECIAL_MODE)
-            setDataForSpecialView();
-        else
-            setDataForSimpleView();
-        //fireTableStructureChanged();
-        //fireTableDataChanged();
+	}
 
-    }
+	/**
+	 * Actualise la table. <br>
+	 * Cette méthode est appelé uniquement quand le MODE vaut
+	 * FileTableModel.SIMPLE_MODE.
+	 */
+	public void setDataForSimpleView() {
+		File[] files = fsm.getFilesList();
 
-    /***************************************************************************
-     * public Class getColumnClass(int col) { int rowIndex = 0; Object o =
-     * getValueAt(rowIndex, col); if (o == null) { return Object.class; } else {
-     * return o.getClass(); } }
-     **************************************************************************/
+		int nbColumns = colNames.size();
+		int nbRows = files.length;
 
-    /**
-     * Récupére la nouvelle URI du méta-modéle et actualise les valeurs Apellé
-     * quand le MODE vaut FileTableModel.SIMPLE_MODE
-     */
-    public void setDataForSimpleView() {
-        File[] files = fsm.getFilesList();
+		Vector data = new Vector(nbRows);
+		Vector rowData = null;
 
-        int nbColumns = colNames.size();
-        int nbRows = files.length;
+		for (int rows = 0; rows < nbRows; rows++) {
 
-        Vector data = new Vector(nbRows);
-        Vector rowData = null;
+			rowData = new Vector(nbColumns);
 
-        for (int rows = 0; rows < nbRows; rows++) {
+			FileDetails fd = new FileDetails(files[rows]);
 
-            rowData = new Vector(nbColumns);
+			for (int cols = 0; cols < nbColumns; cols++) {
+				switch (cols) {
+				case 0:
+					rowData.addElement(files[rows]);
+					break;
+				case 1:
+					rowData.addElement(fd.getSize());
+					break;
 
-            for (int cols = 0; cols < nbColumns; cols++) {
-                switch (cols) {
-                case 0:
-                    rowData.addElement(files[rows]);
-                    break;
-                case 1:
-                    rowData.addElement(new Long(files[rows].length()));
-                    break;
+				case 2:
+					rowData.addElement(fd.getType());
+					break;
 
-                case 2:
-                    rowData.addElement(files[rows].isDirectory() ? "Dossier"
-                            : "Fichier");
-                    break;
+				case 3:
+					rowData.addElement(new Date(files[rows].lastModified()));
+					break;
+				}
+			}
 
-                case 3:
-                    rowData.addElement(new Date(files[rows].lastModified()));
-                    break;
-                }
-            }
+			data.add(rowData);
+		}
 
-            data.add(rowData);
-        }
+		setDataVector(data, colNames);
+	}
 
-        setDataVector(data, colNames);
-    }
+	/**
+	 * Actualise la table. <br>
+	 * Cette méthode est appelé uniquement quand le MODE vaut
+	 * FileTableModel.SPECIAL_MODE.
+	 */
+	public void setDataForSpecialView() {
 
-    /**
-     * Mise à jour de la liste des fichiers pour la vue special (style MacOs)
-     *  
-     */
-    public void setDataForSpecialView() {
+		//Quand clique sur un dossier on retire tout element(s) et on met les
+		//fils de celui sur lequel on vient de cliquer
 
-        //Quand clique sur un dossier on retire tout element(s) et on met les
-        //fils de celui sur lequel on vient de cliquer
-        
-        File[] files = fsm.getFilesList();
+		File[] files = fsm.getFilesList();
 
-        //TODO A TESTER DANS MODERATION PEUT ETRE DES BUGS
-        //Condition pour le cas ou la table ne contient aucune colonne
-        if (!(getColumnCount() == 0))
-            for (int i = lastSelectedColumn + 1; i <= getColumnCount() - 1;)
-                removeColumn(i);
+		//TODO A TESTER DANS MODERATION PEUT ETRE DES BUGS
+		//Condition pour le cas ou la table ne contient aucune colonne
+		if (!(getColumnCount() == 0))
+			for (int i = lastSelectedColumn + 1; i <= getColumnCount() - 1;)
+				removeColumn(i);
 
-        //On ajute la colonne contenant les fichiers à explorer
-        addColumn("", files);
+		//On ajute la colonne contenant les fichiers à explorer
+		addColumn("", files);
 
-    }
+	}
 
-    /**
-     * 
-     * On fixe la valeur de la dérniére colonne selectionnée
-     */
-    public void setSelectedColumn(int column) {
-        lastSelectedColumn = column;
-    }
+	/**
+	 * 
+	 * On fixe la valeur de la dérniére colonne selectionnée
+	 */
+	public void setSelectedColumn(int column) {
+		lastSelectedColumn = column;
+	}
 
-    /**
-     * Renvoie le mode sélectionné
-     */
-    public int getMode() {
-        return MODE;
-    }
+	/**
+	 * Renvoie le mode sélectionné
+	 */
+	public int getMode() {
+		return MODE;
+	}
 
-    public void insertcolumn(JTable table2) {
-        //TableModel1 = (DefaultTableModel) table2.getModel();
-        TableColumn col = new TableColumn();
-        col.setHeaderValue(" ");
-        table2.addColumn(col);
-    }
+	public void insertcolumn(JTable table2) {
+		//TableModel1 = (DefaultTableModel) table2.getModel();
+		TableColumn col = new TableColumn();
+		col.setHeaderValue(" ");
+		table2.addColumn(col);
+	}
 
-    /**
-     * Surcharge car la super-méthode remplit les lignes vides par des valeurs
-     * copiées d'une colonne précedente
-     */
-    public void addColumn(Object columnName, Vector columnData) {
-        columnIdentifiers.addElement(columnName);
+	/**
+	 * Surcharge car la super-méthode remplit les lignes vides par des valeurs
+	 * copiées d'une colonne précedente
+	 */
+	public void addColumn(Object columnName, Vector columnData) {
+		columnIdentifiers.addElement(columnName);
 
-        if (columnData != null) {
+		if (columnData != null) {
 
-            int columnSize = columnData.size();
-            if (columnSize > getRowCount()) {
-                dataVector.setSize(columnSize);//Nombre de ligne + gd
-            }
-            justify(0, getRowCount());
-            int newColumn = getColumnCount() - 1;
-            for (int i = 0; i < getRowCount()/* columnSize */; i++) {
-                Vector row = (Vector) dataVector.elementAt(i);
-                // System.out.println("row = " + row);
-                if (i < columnSize) {
-                    row.setElementAt(columnData.elementAt(i), newColumn);
-                } else if (i >= columnSize) {
-                    row.setElementAt(new String(""), newColumn);
-                }
+			int columnSize = columnData.size();
+			if (columnSize > getRowCount()) {
+				dataVector.setSize(columnSize);//Nombre de ligne + gd
+			}
+			justify(0, getRowCount());
+			int newColumn = getColumnCount() - 1;
+			for (int i = 0; i < getRowCount()/* columnSize */; i++) {
+				Vector row = (Vector) dataVector.elementAt(i);
+				// System.out.println("row = " + row);
+				if (i < columnSize) {
+					row.setElementAt(columnData.elementAt(i), newColumn);
+				} else if (i >= columnSize) {
+					row.setElementAt(new String(""), newColumn);
+				}
 
-            }
-        } else {
-            justify(0, getRowCount());
-        }
+			}
+		} else {
+			justify(0, getRowCount());
+		}
 
-        fireTableStructureChanged();
+		fireTableStructureChanged();
 
-    }
+	}
 
-    /**
-     * Pompé de la superclasse j'avoue j'ai honte !
-     * 
-     * @param from
-     * @param to
-     */
-    private void justify(int from, int to) {
-        // Sometimes the DefaultTableModel is subclassed
-        // instead of the AbstractTableModel by mistake.
-        // Set the number of rows for the case when getRowCount
-        // is overridden.
-        dataVector.setSize(getRowCount());
+	/**
+	 * Pompé de la superclasse j'avoue j'ai honte !
+	 * 
+	 * @param from
+	 * @param to
+	 */
+	private void justify(int from, int to) {
+		// Sometimes the DefaultTableModel is subclassed
+		// instead of the AbstractTableModel by mistake.
+		// Set the number of rows for the case when getRowCount
+		// is overridden.
+		dataVector.setSize(getRowCount());
 
-        for (int i = from; i < to; i++) {
-            if (dataVector.elementAt(i) == null) {
-                dataVector.setElementAt(new Vector(), i);
-            }
-            ((Vector) dataVector.elementAt(i)).setSize(getColumnCount());
-        }
-    }
+		for (int i = from; i < to; i++) {
+			if (dataVector.elementAt(i) == null) {
+				dataVector.setElementAt(new Vector(), i);
+			}
+			((Vector) dataVector.elementAt(i)).setSize(getColumnCount());
+		}
+	}
 
-    /**
-     * Retire une colonne du modèle
-     * 
-     * @param columnIndex
-     *            L'index de la colonne à retirer
-     */
-    public void removeColumn(int columnIndex) {
+	/**
+	 * Retire une colonne du modèle
+	 * 
+	 * @param columnIndex
+	 *            L'index de la colonne à retirer
+	 */
+	public void removeColumn(int columnIndex) {
 
-        int oldColumnCount = getColumnCount();
-        //On retire toutes les valeurs de la colonne columnIndex
-        for (int row = 0; row < getColumnCount(); row++) {
-            Vector rowVector = (Vector) dataVector.elementAt(row);
-            rowVector.removeElementAt(columnIndex);
-        }
-        this.setColumnCount(oldColumnCount - 1);
-    }
+		int oldColumnCount = getColumnCount();
+		//On retire toutes les valeurs de la colonne columnIndex
+		for (int row = 0; row < getColumnCount(); row++) {
+			Vector rowVector = (Vector) dataVector.elementAt(row);
+			rowVector.removeElementAt(columnIndex);
+		}
+		this.setColumnCount(oldColumnCount - 1);
+	}
 
-    /**
-     * @inheritDoc
-     */
-    public boolean isCellEditable(int row, int column) {
-        return false;
-    }
+	/**
+	 * @inheritDoc
+	 */
+	public boolean isCellEditable(int row, int column) {
+		return false;
+	}
 }
--- FSeeker/src/renderer/FileTableCellRenderer.java
+++ FSeeker/src/renderer/FileTableCellRenderer.java
@@ -1,97 +1,73 @@
 /*
  * Créé le 20 oct. 2004
- *
- * Classe de rendu graphique pour une JTable
  */
 package renderer;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.FlowLayout;
 import java.io.File;
+import java.util.Date;
 
-import java.awt.Dimension;
-
-import java.text.SimpleDateFormat;
-
-//import javax.swing.UIManager;
-
-import javax.swing.Icon;
 import javax.swing.JLabel;
 import javax.swing.JTable;
-import java.util.*;
-
 import javax.swing.table.DefaultTableCellRenderer;
 
-//import misc.FileUtilities;
-
 import misc.ImagesMap;
-
-//import java.util.*;
-
-//import misc.file.FileUtilities;
+import misc.file.FileUtilities.FileDetails;
 
 /**
+ * Classe de rendu graphique pour la vue de détails.
  * 
  * @author aitelhab
+ * @author Sted
  */
 public class FileTableCellRenderer extends DefaultTableCellRenderer {
-    
 
- 
-   //TODO Retirer de cette classe (dans misc car utilisé ailleurs, à voir ?)
-    protected static SimpleDateFormat dateFormat = new SimpleDateFormat(
-            "EEEEEEEE dd MMMMMMMMM yyyy 'à' hh:mm:ss", Locale
-                    .getDefault());
-    
-    
-    public Component getTableCellRendererComponent(JTable table, Object value,
-            boolean isSelected, boolean hasFocus, int row, int column) {
+	public Component getTableCellRendererComponent(JTable table, Object value,
+			boolean isSelected, boolean hasFocus, int row, int column) {
 
-        if (!isSelected) {
-            Color c = table.getBackground();
-            if ((row % 2) == 0 && c.getRed() > 20 && c.getGreen() > 20
-                    && c.getBlue() > 20)
-                setBackground(new Color(c.getRed() - 20, c.getGreen() - 20, c
-                        .getBlue() - 20));
-            else
-                setBackground(c);
-        }      
-          
-        return super.getTableCellRendererComponent(table, value, isSelected,
-                hasFocus, row, column);
-    }
+		super.getTableCellRendererComponent(table, value, isSelected, hasFocus,
+				row, column);
 
-    public void setValue(Object value) {
-        if (value != null) {
-            if (value instanceof File) {
-                File file = (File) value;
-                Icon fileIcon = ImagesMap.get16x16(file);
-                setHorizontalAlignment(JLabel.LEFT);               
-                setIcon(fileIcon);
-                setForeground(Color.black);                
-                setToolTipText(null);//FileUtilities.getDetails(file)); 
-                super.setValue(file.getName() + (file.isDirectory() ? "/" : ""));
-                return ;
-            } else if (value instanceof Date) {              
-               String newValue = dateFormat.format((Date)value);
-               setHorizontalAlignment(JLabel.LEFT);
-               setForeground(Color.black);
-               setIcon(null);
-               super.setValue(newValue);
-            } else if (value instanceof Long) {
-                setIcon(null);
-                setForeground(Color.black);
-                setHorizontalAlignment(JLabel.CENTER);
-                super.setValue(value+ " octets ");
-            } else {
-                setIcon(null);
-                setHorizontalAlignment(JLabel.CENTER);
-                setForeground(Color.black);
-                super.setText(value.toString());
-            }
-        }
-        
-    }
+		if (!isSelected) {
+			Color c = table.getBackground();
+
+			// Faudra que tu m'explique pour ces get*, ça fait pareil sans :D
+			// && c.getRed() > 20 && c.getGreen() > 20 && c.getBlue() > 20
+			if ((row % 2) == 0)
+				setBackground(new Color(c.getRGB() - 5460)); // Mouahah
+			else
+				setBackground(c);
+		}
+
+		return this;
+	}
+
+	public void setValue(Object value) {
+
+		if (value != null) {
+
+			if (value instanceof File) {
+				File file = (File) value;
+				FileDetails fd = new FileDetails(file);
+				setHorizontalAlignment(JLabel.LEFT);
+				setIcon(ImagesMap.get16x16(file));
+				setToolTipText(fd.getToolTip());
+				setFont(fd.getFont());
+				setText(fd.getName());
+
+			} else if (value instanceof Date) {
+				setHorizontalAlignment(JLabel.LEFT);
+				setText(FileDetails.dateFormat.format((Date) value));
+				setIcon(null);
+
+			} else {
+				setHorizontalAlignment(JLabel.CENTER);
+				setText(value.toString());
+				setIcon(null);
+			}
+
+		}
+	}
 
 }
