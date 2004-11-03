702caf7356a2319f474bf52d72464492fec94e7e
sted <sted>
2004-11-03 11:39:18 +0000

    me rappeler de toucher tout ce chaos

--- FSeeker/src/model/FileTableModel.java
+++ FSeeker/src/model/FileTableModel.java
@@ -7,17 +7,12 @@ package model;
 
 import java.io.File;
 import java.util.Date;
-import java.util.Enumeration;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Vector;
 
-import javax.swing.JTable;
-import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableColumn;
 
-import gui.FileTableGUI;
 import misc.file.FileUtilities.FileDetails;
 
 /**
@@ -29,288 +24,302 @@ import misc.file.FileUtilities.FileDetails;
  */
 public class FileTableModel extends DefaultTableModel implements Observer {
 
-    public static int SIMPLE_MODE = 0;
+	/** Mode simple détails */
+	public static int SIMPLE_MODE = 0;
 
-    public static int SPECIAL_MODE = 1;
+	/** Mode à la MacOS */
+	public static int SPECIAL_MODE = 1;
 
-    private int MODE = -1;
+	/** Mode par défaut */
+	protected int MODE = SIMPLE_MODE;
 
-    /* La derniére colonne selectionnée */
-    private int lastSelectedColumn = -1;
+	/** La dernière colonne sélectionnée */
+	protected int lastSelectedColumn = -1;
 
-    protected Vector colNames = new Vector(4);
+	/** Les colonnes */
+	protected Vector colNames = new Vector(4);
+	{
+		colNames.addElement("Nom du fichier");
+		colNames.addElement("Taille");
+		colNames.addElement("Type");
+		colNames.addElement("Date de Modification");
+	}
 
-    {
-        colNames.addElement("Nom du fichier");
-        colNames.addElement("Taille");
-        colNames.addElement("Type");
-        colNames.addElement("Date de Modification");
-    }
+	/** Le supra-modèle */
+	protected FSeekerModel fsm = null;
 
-    protected FSeekerModel fsm = null;
+	/**
+	 * Constructeur d'un modèle de table.
+	 * 
+	 * @param fsm
+	 *            Le supra-modèle associé
+	 * @param mode
+	 *            Le mode choix entre FileTableModel.SPECIAL_MODE et
+	 *            FileTableModel.SIMPLE_MODE
+	 */
+	public FileTableModel(FSeekerModel fsm, int mode) {
+		this.fsm = fsm;
+		fsm.addObserver(this);
+		this.MODE = mode;
 
-    /**
-     * Contructeur d'un model
-     * 
-     * @param fsm
-     *            Le supra-modèle associé
-     * @param mode
-     *            Le mode choix entre FileTableModel.SPECIAL_MODE et
-     *            FileTableModel.SIMPLE_MODE
-     */
-    public FileTableModel(FSeekerModel fsm, int mode) {
-        this.fsm = fsm;
-        fsm.addObserver(this);
-        this.MODE = mode;
+		if (MODE == SPECIAL_MODE)
+			setDataForSpecialView();
+		else {
+			setColumnIdentifiers(colNames);
+			setDataForSimpleView();
+		}
 
-        if (MODE == SPECIAL_MODE)
-            setDataForSpecialView();
-        else {
-            setColumnIdentifiers(colNames);
-            setDataForSimpleView();
-        }
+	}
 
-    }
+	/**
+	 * Retourne le supra-modèle associé.
+	 * 
+	 * @return supra-modèle associé
+	 */
+	public FSeekerModel getModel() {
+		return fsm;
+	}
 
-    public FSeekerModel getModel() {
-        return fsm;
-    }
+	/**
+	 * Appelée quand le supra-modèle est modifiée.
+	 */
+	public void update(Observable o, Object arg) {
+		if (MODE == SPECIAL_MODE)
+			setDataForSpecialView();
+		else
+			setDataForSimpleView();
 
-    public void update(Observable o, Object arg) {
-        //Mise à jour des données
-        if (MODE == SPECIAL_MODE)
-            setDataForSpecialView();
-        else
-            setDataForSimpleView();
-        //fireTableStructureChanged();
-        fireTableDataChanged();
-    }
+		fireTableDataChanged();
+	}
 
-    /**
-     * Actualise la table. <br>
-     * Cette méthode est appelé uniquement quand le MODE vaut
-     * FileTableModel.SIMPLE_MODE.
-     */
-    public void setDataForSimpleView() {
+	/**
+	 * Actualise la table. <br>
+	 * Cette méthode est appelé uniquement quand le MODE vaut
+	 * FileTableModel.SIMPLE_MODE.
+	 */
+	public void setDataForSimpleView() {
 
-        File[] files = fsm.getFilesList();
+		File[] files = fsm.getFilesList();
 
-        if (files == null) {
-            setDataVector(null, colNames);
-            return;
-        }
+		if (files == null) {
+			setDataVector(null, colNames);
+			return;
+		}
 
-        int nbColumns = colNames.size();
-        int nbRows = files.length;
+		int nbColumns = colNames.size();
+		int nbRows = files.length;
 
-        Vector data = new Vector(nbRows);
-        Vector rowData = null;
+		Vector data = new Vector(nbRows);
+		Vector rowData = null;
 
-        for (int rows = 0; rows < nbRows; rows++) {
+		for (int rows = 0; rows < nbRows; rows++) {
 
-            rowData = new Vector(nbColumns);
+			rowData = new Vector(nbColumns);
 
-            FileDetails fd = new FileDetails(files[rows]);
+			FileDetails fd = new FileDetails(files[rows]);
 
-            for (int cols = 0; cols < nbColumns; cols++) {
-                switch (cols) {
-                case 0:
-                    rowData.addElement(files[rows]);
-                    break;
-                case 1:
-                    rowData.addElement(fd.getSize());
-                    break;
+			for (int cols = 0; cols < nbColumns; cols++) {
+				switch (cols) {
+				case 0:
+					rowData.addElement(files[rows]);
+					break;
+				case 1:
+					rowData.addElement(fd.getSize());
+					break;
 
-                case 2:
-                    rowData.addElement(fd.getType());
-                    break;
+				case 2:
+					rowData.addElement(fd.getType());
+					break;
 
-                case 3:
-                    rowData.addElement(new Date(files[rows].lastModified()));
-                    break;
-                }
-            }
+				case 3:
+					rowData.addElement(new Date(files[rows].lastModified()));
+					break;
+				}
+			}
 
-            data.add(rowData);
-        }
+			data.add(rowData);
+		}
 
-        setDataVector(data, colNames);
-    }
+		setDataVector(data, colNames);
+	}
 
-    /**
-     * Renvoie le mode sélectionné
-     */
-    public int getMode() {
-        return MODE;
-    }
+	/**
+	 * Renvoie le mode du modèle.
+	 * 
+	 * @return SPECIAL_MODE ou SIMPLE_MODE
+	 */
+	public int getMode() {
+		return MODE;
+	}
 
-    /**
-     * Actualise la table. <br>
-     * Cette méthode est appelé uniquement quand le MODE vaut
-     * FileTableModel.SPECIAL_MODE.
-     */
-    public void setDataForSpecialView() {
+	// TODO J'AI JAMAIS VU UN MAXI BORDEL PAREIL. Toute la suite du fichier est
+	// chaotique putain, tu m'étonnes que y'a des bugs.
 
-        //Quand clique sur un dossier on retire tout element(s) et on met les
-        //fils de celui sur lequel on vient de cliquer
+	/**
+	 * Actualise la table. <br>
+	 * Cette méthode est appelé uniquement quand le MODE vaut
+	 * FileTableModel.SPECIAL_MODE.
+	 */
+	public void setDataForSpecialView() {
 
-        File[] files = fsm.getFilesList();
+		//Quand clique sur un dossier on retire tout element(s) et on met les
+		//fils de celui sur lequel on vient de cliquer
 
-        //TODO A TESTER DANS MODERATION PEUT ETRE DES BUGS
-        //Condition pour le cas ou la table ne contient aucune colonne cad au
-        // demarrage
-        if (!(getColumnCount() == 0))
-            for (int i = lastSelectedColumn + 1; i <= getColumnCount() - 1;) {
-                removeColumn(i);                
-            }
+		File[] files = fsm.getFilesList();
 
-        //On ajoute la colonne contenant les fichiers à explorer
-        addColumn("", files);
+		//TODO A TESTER DANS MODERATION PEUT ETRE DES BUGS
+		//Condition pour le cas ou la table ne contient aucune colonne cad au
+		// demarrage
+		if (!(getColumnCount() == 0))
+			for (int i = lastSelectedColumn + 1; i <= getColumnCount() - 1;) {
+				removeColumn(i);
+			}
 
-    }
+		//On ajoute la colonne contenant les fichiers à explorer
+		addColumn("", files);
 
-    /**
-     * 
-     * On fixe la valeur de la dérniére colonne selectionnée
-     */
-    public void setSelectedColumn(int column) {
-        lastSelectedColumn = column;
-    }
+	}
 
-   /** public void insertcolumn(JTable table2) {
-        //TableModel1 = (DefaultTableModel) table2.getModel();
-        TableColumn col = new TableColumn();
-        col.setHeaderValue(" ");
-        table2.addColumn(col);
-    }**/
+	/**
+	 * 
+	 * On fixe la valeur de la dérniére colonne selectionnée
+	 */
+	public void setSelectedColumn(int column) {
+		lastSelectedColumn = column;
+	}
 
-    /**
-     * >>>>>>> 1.12 Surcharge car la super-méthode remplit les lignes (valeur
-     * null) vides par des valeurs copiées d'une colonne précedente
-     */
-    public void addColumn(Object columnName, Vector columnData) {
-        columnIdentifiers.addElement(columnName);
+	/***************************************************************************
+	 * public void insertcolumn(JTable table2) { //TableModel1 =
+	 * (DefaultTableModel) table2.getModel(); TableColumn col = new
+	 * TableColumn(); col.setHeaderValue(" "); table2.addColumn(col); }
+	 **************************************************************************/
 
-        if (columnData != null) {
+	/**
+	 * >>>>>>> 1.12 Surcharge car la super-méthode remplit les lignes (valeur
+	 * null) vides par des valeurs copiées d'une colonne précedente
+	 */
+	public void addColumn(Object columnName, Vector columnData) {
+		columnIdentifiers.addElement(columnName);
 
-            //Nombre de lignes (changera si contient plus de lignes que
-            // dataVector (vecteur des données))
-            int oldRowCount = getRowCount();
-           
-            //Nombre de lignes du vecteur à ajouter
-            int columnDataRowCount = columnData.size();
+		if (columnData != null) {
 
-            //Si le tableau à ajouter contient plus de ligne que le courant
-            // alors on aggrandit la taille du tableau courant
-            if (columnDataRowCount > getRowCount())
-                dataVector.setSize(columnDataRowCount);
+			//Nombre de lignes (changera si contient plus de lignes que
+			// dataVector (vecteur des données))
+			int oldRowCount = getRowCount();
 
-            //On initialise chaque chaque ligne du tableau courant (pas de
-            // valeur null)
-            justify(0, getRowCount());
+			//Nombre de lignes du vecteur à ajouter
+			int columnDataRowCount = columnData.size();
 
-            //Indice de la nouvelle colonne
-            int newColumn = getColumnCount() - 1;            
+			//Si le tableau à ajouter contient plus de ligne que le courant
+			// alors on aggrandit la taille du tableau courant
+			if (columnDataRowCount > getRowCount())
+				dataVector.setSize(columnDataRowCount);
 
-            for (int i = 0; i < getRowCount()/* columnSize */; i++) {
-                Vector row = (Vector) dataVector.elementAt(i);
-                // System.out.println("row = " + row);
-                if (i < columnDataRowCount) {
-                    row.setElementAt(columnData.elementAt(i), newColumn);
-                } else if (i >= columnDataRowCount) {
-                    row.setElementAt(new String(""), newColumn);
-                }
+			//On initialise chaque chaque ligne du tableau courant (pas de
+			// valeur null)
+			justify(0, getRowCount());
 
-                // On remplaces les null par des ""
-                if (columnDataRowCount > oldRowCount && i >= oldRowCount
-                        && getColumnCount() > 1)
-                    row.setElementAt("", newColumn - 1);
-            }
+			//Indice de la nouvelle colonne
+			int newColumn = getColumnCount() - 1;
 
-        } else {
-            justify(0, getRowCount());
-        }
+			for (int i = 0; i < getRowCount()/* columnSize */; i++) {
+				Vector row = (Vector) dataVector.elementAt(i);
+				// System.out.println("row = " + row);
+				if (i < columnDataRowCount) {
+					row.setElementAt(columnData.elementAt(i), newColumn);
+				} else if (i >= columnDataRowCount) {
+					row.setElementAt(new String(""), newColumn);
+				}
 
-        // On retire les lignes ne contenant rien TODO BUG ?
-        //removeEmptyRows();
-        
-        //System.out.println(dataVector);
-        this.fireTableDataChanged();
-        fireTableStructureChanged();
-        
+				// On remplaces les null par des ""
+				if (columnDataRowCount > oldRowCount && i >= oldRowCount
+						&& getColumnCount() > 1)
+					row.setElementAt("", newColumn - 1);
+			}
 
-    }
+		} else {
+			justify(0, getRowCount());
+		}
 
-    /* Retire les lignes vide */
-    private void removeEmptyRows() {
-        //On supprime toutes les lignes vides            
-        for (int row = getRowCount() - 1 ; row > 0;  row--)
-            if (isEmptyRow(row))             
-                dataVector.removeElementAt(row);       
-    }
+		// On retire les lignes ne contenant rien TODO BUG ?
+		//removeEmptyRows();
 
-    /* Si la ligne ne contient que des éléments vides renvoie vrai */
-    private boolean isEmptyRow(int row) {
-        int colNumber = getColumnCount();
-        boolean isEmpty = true;
-        for (int col = 0; col < colNumber; col++) {
-            Vector rowData = (Vector) dataVector.elementAt(row);
-            /**Object colData = null;           
-            if (!(rowData == null))
-                colData =  rowData.elementAt(col);
-            if (!(colData == null))**/
-            System.out.println("rowData "+rowData);
-                if (!(rowData.elementAt(col).toString().equals("") && isEmpty))
-                    isEmpty = false;
-        }        
-        return isEmpty;
+		//System.out.println(dataVector);
+		this.fireTableDataChanged();
+		fireTableStructureChanged();
 
-    }
+	}
 
-    /*
-     * Initialise un ensemble de ligne. (Bon nombre de colonne pour chaque ligne
-     * et valeur non null)
-     * 
-     * @param from Premiére ligne à initialiser
-     * 
-     * @param to Derniére ligne à initialiser
-     */
-    private void justify(int from, int to) {
-        // Sometimes the DefaultTableModel is subclassed
-        // instead of the AbstractTableModel by mistake.
-        // Set the number of rows for the case when getRowCount
-        // is overridden.
-        dataVector.setSize(getRowCount());
+	/* Retire les lignes vide */
+	private void removeEmptyRows() {
+		//On supprime toutes les lignes vides
+		for (int row = getRowCount() - 1; row > 0; row--)
+			if (isEmptyRow(row))
+				dataVector.removeElementAt(row);
+	}
 
-        for (int i = from; i < to; i++) {
-            if (dataVector.elementAt(i) == null) {
-                dataVector.setElementAt(new Vector(), i);
-            }
-            ((Vector) dataVector.elementAt(i)).setSize(getColumnCount());
-        }
-    }
+	/* Si la ligne ne contient que des éléments vides renvoie vrai */
+	private boolean isEmptyRow(int row) {
+		int colNumber = getColumnCount();
+		boolean isEmpty = true;
+		for (int col = 0; col < colNumber; col++) {
+			Vector rowData = (Vector) dataVector.elementAt(row);
+			/*******************************************************************
+			 * Object colData = null; if (!(rowData == null)) colData =
+			 * rowData.elementAt(col); if (!(colData == null))
+			 ******************************************************************/
+			System.out.println("rowData " + rowData);
+			if (!(rowData.elementAt(col).toString().equals("") && isEmpty))
+				isEmpty = false;
+		}
+		return isEmpty;
 
-    /**
-     * Retire une colonne du modèle
-     * 
-     * @param columnIndex
-     *            L'index de la colonne à retirer
-     */
-    public void removeColumn(int columnIndex) {
+	}
 
-        int oldColumnCount = getColumnCount();
-        //On retire toutes les valeurs de la colonne columnIndex
-        for (int row = 0; row < getColumnCount(); row++) {
-            Vector rowVector = (Vector) dataVector.elementAt(row);
-            rowVector.removeElementAt(columnIndex);
-        }
-        this.setColumnCount(oldColumnCount - 1);
-    }
+	/*
+	 * Initialise un ensemble de ligne. (Bon nombre de colonne pour chaque ligne
+	 * et valeur non null)
+	 * 
+	 * @param from Premiére ligne à initialiser
+	 * 
+	 * @param to Derniére ligne à initialiser
+	 */
+	private void justify(int from, int to) {
+		// Sometimes the DefaultTableModel is subclassed
+		// instead of the AbstractTableModel by mistake.
+		// Set the number of rows for the case when getRowCount
+		// is overridden.
+		dataVector.setSize(getRowCount());
 
-    /**
-     * @inheritDoc
-     */
-    public boolean isCellEditable(int row, int column) {
-        return false;
-    }
+		for (int i = from; i < to; i++) {
+			if (dataVector.elementAt(i) == null) {
+				dataVector.setElementAt(new Vector(), i);
+			}
+			((Vector) dataVector.elementAt(i)).setSize(getColumnCount());
+		}
+	}
+
+	/**
+	 * Retire une colonne du modèle
+	 * 
+	 * @param columnIndex
+	 *            L'index de la colonne à retirer
+	 */
+	public void removeColumn(int columnIndex) {
+
+		int oldColumnCount = getColumnCount();
+		//On retire toutes les valeurs de la colonne columnIndex
+		for (int row = 0; row < getColumnCount(); row++) {
+			Vector rowVector = (Vector) dataVector.elementAt(row);
+			rowVector.removeElementAt(columnIndex);
+		}
+		this.setColumnCount(oldColumnCount - 1);
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public boolean isCellEditable(int row, int column) {
+		return false;
+	}
 }
