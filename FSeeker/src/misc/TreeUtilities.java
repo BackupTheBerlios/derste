f65c361f8da77267f8a2492a10c46442da12356f
sted <sted>
2004-11-03 11:27:29 +0000

    doc

--- FSeeker/src/misc/TreeUtilities.java
+++ FSeeker/src/misc/TreeUtilities.java
@@ -9,11 +9,21 @@ import java.util.LinkedList;
 import javax.swing.tree.TreePath;
 
 /**
+ * Classe utilitaire regroupant les méthodes appliquées sur des arbres.
+ * 
  * @author sted
  */
 public class TreeUtilities {
+	
+	/**
+	 * Retourne le TreePath d'un fichier.
+	 * 
+	 * @param f un fichier
+	 * @return son TreePath
+	 */
 	public static TreePath getTreePath(File f) {
 
+		// On empile tout d'un côté...
 		LinkedList filesList = new LinkedList();
 		File g = new File(f.toString());
 
@@ -24,6 +34,7 @@ public class TreeUtilities {
 
 		TreePath treepath = new TreePath(filesList.removeLast());
 
+		// ... et on ressort tout de l'autre sens !
 		while (!filesList.isEmpty())
 			treepath = treepath.pathByAddingChild(filesList.removeLast());
 
