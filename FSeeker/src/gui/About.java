effb058a97b40e1988236d6df79917467c090488
brahima <brahima>
2004-11-11 23:05:19 +0000

    //No modif

--- FSeeker/src/gui/About.java
+++ FSeeker/src/gui/About.java
@@ -16,8 +16,14 @@ import misc.ImagesMap;
  * @author sted
  */
 public class About {
-	public About(Frame owner) {
-		Icon fseeker = ImagesMap.get(ImagesMap.FSEEKER_LOGO);
-		JOptionPane.showMessageDialog(owner, "<html><h2>FSeeker v" + FSeeker.VERSION + " </h2><br><hr><br>Réalisé par <ul><li>Stéphane D. <img src=\"moi.jpg\"></li><li>Brahim A. E. <img src=\"lui.jpg\"></li></ul></html>", "A propos", JOptionPane.INFORMATION_MESSAGE, fseeker);
-	}
+    public About(Frame owner) {
+        Icon fseeker = ImagesMap.get(ImagesMap.FSEEKER_LOGO);
+        JOptionPane
+                .showMessageDialog(
+                        owner,
+                        "<html><h2>FSeeker v"
+                                + FSeeker.VERSION
+                                + " </h2><br><hr><br>Réalisé par <ul><li>Stéphane D. <img src=\"moi.jpg\"></li><li>Brahim A. E. <img src=\"lui.jpg\"></li></ul></html>",
+                        "A propos", JOptionPane.INFORMATION_MESSAGE, fseeker);
+    }
 }
